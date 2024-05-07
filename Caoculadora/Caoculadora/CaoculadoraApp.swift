9862fb9a4cc81b31247c430e89fb7ca37a9d6486
Maria Clara Albuquerque Moura <amouramariaclara@gmail.com>
2024-05-07 16:12:21 -0300

    Adiciona arquivos do projeto

---
+++ Caoculadora/Caoculadora/CaoculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaoculadoraApp.swift
+//  Caoculadora
+//
+//  Created by Maria Clara Albuquerque Moura on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CaoculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Caoculadora
+//
+//  Created by Maria Clara Albuquerque Moura on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "pawprint")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
