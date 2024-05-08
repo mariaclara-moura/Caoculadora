304d862217ef3da90faf76484b34b34d83ace08b
Maria Clara Albuquerque Moura <amouramariaclara@gmail.com>
2024-05-08 18:02:54 -0300

    Adiciona imagem e funcionalidade ao botão

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -8,21 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var years: Int? = nil
-    @State var months: Int? = nil
+    @State var years: Int?
+    @State var months: Int?
+    @State var result: Int?
+
     var body: some View {
         VStack (alignment: .leading) {
-            Text("Qual a idade do seu cão?")
+            Text("Qual a idade do seu cão?").font(.system(size: 24))
             Text("Anos")
             TextField("Digite quantos anos seu cão tem.", value: $years, format: .number)
             Text("Meses")
             TextField("E quantos meses além disso ele tem.", value: $months, format: .number)
+            Text("Porte")
+            if let result {
+                Text("Seu cachorro tem, em idade humana...")
+                Text("\(result) anos")
+            } else {
+//                EmptyView()
+                Image(ImageResource.clarinha).resizable().scaledToFit().frame(maxWidth:.infinity, maxHeight: 150).shadow(radius: 20)
+            }
+            Button(action:  {result = 23}, label: {
+                ZStack{
+                    Color.indigo
+                    Text("Cãocular").foregroundStyle(.white)
+
+                }
+            }).cornerRadius(10).frame(height: 50.0)
         }
-        .textFieldStyle(.roundedBorder).keyboardType(.numberPad).bold().fontDesign(.rounded)
+        .textFieldStyle(.customRound(strokeSize: 1))
+        .keyboardType(.numberPad)
+        .bold()
+        .fontDesign(.rounded)
         .padding()
+        .foregroundStyle(Color.indigo)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct CustomStrokeTextFieldStyle: TextFieldStyle {
+    let strokeSize: Double
+    
+    init(strokeSize: Double = 2) {
+        self.strokeSize = strokeSize
+    }
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+           .padding(10)
+           .background(
+                RoundedRectangle(cornerRadius: 10, style:.continuous)
+                    .stroke(Color.indigo.opacity(0.3), lineWidth: strokeSize)
+            )
+    }
+}
+
+extension TextFieldStyle where Self == CustomStrokeTextFieldStyle {
+    static func customRound(strokeSize: Double = 2) -> Self {
+        CustomStrokeTextFieldStyle(strokeSize: strokeSize)
+    }
 }
