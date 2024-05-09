b71289b8b2a362588628d9e04a390d905977d508
Maria Clara Albuquerque Moura <amouramariaclara@gmail.com>
2024-05-09 17:49:02 -0300

    Adiciona Segmented Control e Implementa função de cálculo da idade

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -11,15 +11,25 @@ struct ContentView: View {
     @State var years: Int?
     @State var months: Int?
     @State var result: Int?
+    let scales = ["Pequeno","Médio","Grande"]
+    @State var scale: String = "Pequeno"
 
     var body: some View {
         VStack (alignment: .leading) {
+//            Spacer()
             Text("Qual a idade do seu cão?").font(.system(size: 24))
             Text("Anos")
             TextField("Digite quantos anos seu cão tem.", value: $years, format: .number)
             Text("Meses")
             TextField("E quantos meses além disso ele tem.", value: $months, format: .number)
             Text("Porte")
+            Picker("Porte", selection: $scale){
+                ForEach(scales, id: \.self) { scale in
+                    Text(scale).tag(scale)
+                }
+            }.pickerStyle(.segmented).padding()
+            Divider().background(.indigo)
+            Spacer()
             if let result {
                 Text("Seu cachorro tem, em idade humana...")
                 Text("\(result) anos")
@@ -27,7 +37,8 @@ struct ContentView: View {
 //                EmptyView()
                 Image(ImageResource.clarinha).resizable().scaledToFit().frame(maxWidth:.infinity, maxHeight: 150).shadow(radius: 20)
             }
-            Button(action:  {result = 23}, label: {
+            Spacer()
+            Button(action:  processYears, label: {
                 ZStack{
                     Color.indigo
                     Text("Cãocular").foregroundStyle(.white)
@@ -35,12 +46,28 @@ struct ContentView: View {
                 }
             }).cornerRadius(10).frame(height: 50.0)
         }
-        .textFieldStyle(.customRound(strokeSize: 1))
+        .textFieldStyle(.customRound(strokeSize: 1.5))
+//        .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad)
         .bold()
         .fontDesign(.rounded)
         .padding()
         .foregroundStyle(Color.indigo)
+    }
+    func processYears(){
+        
+        guard let years, let months else {
+            //se eu nao consegui desempacotar years e months:
+            print("Preencha o campo de entrada.")
+            return
+        } 
+        guard years > 0 || months > 0 else {
+            //se eu nao consegui desempacotar years e months:
+            print("Algum dos campos deve ser maior que zero.")
+            return
+        }
+
+        result = (years * 7) + ( months * 7 / 12)
     }
 }
 
@@ -69,3 +96,6 @@ extension TextFieldStyle where Self == CustomStrokeTextFieldStyle {
         CustomStrokeTextFieldStyle(strokeSize: strokeSize)
     }
 }
+
+
+//(Color(red: 0x4F/0xFF, green: 0x46/0xFF, blue: 0xE5/0xFF))
