603b4e3d8b9e76a0b965a4f28e3e09247eacd7b5
Maria Clara Albuquerque Moura <amouramariaclara@gmail.com>
2024-05-13 17:10:42 -0300

    Implementa alertas e animações, ajusta layout e cores

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -11,125 +11,163 @@ struct ContentView: View {
     @State var years: Int?
     @State var months: Int?
     @State var result: Int?
-//    let scales = [
-//        "Pequeno",
-//        "Médio",
-//        "Grande"
-//    ]
-//    @State var scale: String = "Pequeno"
+    @State var failedInput: Bool = false
+    let tituloPreencherCampos = "Preencha os campos corretamente para poder Cãocular!"
     @State var porteSelecionado: Porte = .pequeno
     
     var body: some View {
-        VStack (
-            alignment: .leading
-        ) {
-            Spacer()
-            Text(
-                "Qual a idade do seu cão?"
-            ).font(
-                .header5
-            )
-            Text(
-                "Anos"
-            ).font(
-                .body1
-            )
-            TextField(
-                "Digite quantos anos seu cão tem.",
-                value: $years,
-                format: .number
-            )
-            Text(
-                "Meses"
-            ).font(
-                .body1
-            )
-            TextField(
-                "E quantos meses além disso ele tem.",
-                value: $months,
-                format: .number
-            )
-            Text(
-                "Porte"
-            ).font(
-                .body1
-            )
-            Picker(
-                "Porte",
-                selection: $porteSelecionado
-            ){
-                ForEach(
-                    Porte.allCases,
-                    id: \.self
-                ) { scale in
+        NavigationStack {
+            ScrollView{
+                VStack (
+                    alignment: .leading,
+                    spacing: 20.0
+                ) {
                     Text(
-                        scale.rawValue.capitalized
-                    ).tag(
-                        scale
-                    )
-                }
-            }.pickerStyle(
-                .segmented
-            ).padding()
-            Divider().background(
-                .indigo
-            )
-            Spacer()
-            if let result {
-                Text(
-                    "Seu cachorro tem, em idade humana..."
-                ).font(
-                    .body1
-                )
-                Text(
-                    "\(result) anos"
-                ).font(
-                    .display
-                )
-            } else {
-                //                EmptyView()
-                Image(
-                    ImageResource.clarinha
-                ).resizable().scaledToFit().frame(
-                    maxWidth:.infinity,
-                    maxHeight: 150
-                ).shadow(
-                    radius: 20
-                )
-            }
-            Spacer()
-            Button(action:  processYears,
-                   label: {
-                ZStack{
-                    Color.indigo
-                    Text(
-                        "Cãocular"
-                    ).foregroundStyle(
-                        .white
+                        "Qual a idade do seu cão?"
                     ).font(
-                        .body1
+                        .header5
+                    ).padding(
+                        .top,
+                        24.0
                     )
-                    
+                    VStack(
+                        alignment: .leading,
+                        spacing: 8.0
+                    ){
+                        Text(
+                            "Anos"
+                        ).font(
+                            .body1
+                        )
+                        TextField(
+                            "Digite quantos anos seu cão tem.",
+                            value: $years,
+                            format: .number
+                        )}
+                    VStack(
+                        alignment: .leading,
+                        spacing: 8.0
+                    ){
+                        Text(
+                            "Meses"
+                        ).font(
+                            .body1
+                        )
+                        TextField(
+                            "E quantos meses além disso ele tem.",
+                            value: $months,
+                            format: .number
+                        )}
+                    VStack(
+                        alignment: .leading,
+                        spacing: 8.0
+                    ){
+                        Text(
+                            "Porte"
+                        ).font(
+                            .body1
+                        )
+                        Picker(
+                            "Porte",
+                            selection: $porteSelecionado
+                        ){
+                            ForEach(
+                                Porte.allCases,
+                                id: \.self
+                            ) { scale in
+                                Text(
+                                    scale.rawValue.capitalized
+                                ).tag(
+                                    scale
+                                )
+                            }
+                        }.pickerStyle(
+                            .segmented
+                        )}
+                    Divider().background(
+                        .indigo600
+                    )
+                    Spacer()
+                    if let result {
+                        Text(
+                            "Seu cachorro tem, em idade humana..."
+                        ).font(
+                            .body1
+                        ).frame(
+                            maxWidth:.infinity
+                        )
+                        Text(
+                            "\(result) anos"
+                        ).contentTransition(.numericText()).font(
+                            .display
+                        ).frame(
+                            maxWidth:.infinity
+                        )
+                    } else {
+                        //                EmptyView()
+                        Image(
+                            ImageResource.clarinha
+                        ).resizable().scaledToFit().frame(
+                            maxWidth:.infinity,
+                            maxHeight: 150
+                        ).shadow(
+                            radius: 20
+                        )
+                    }
+                    Spacer()
+                    Button(action:  processYears,
+                           label: {
+                        ZStack{
+                            Color.indigo600
+                            Text(
+                                "Cãocular"
+                            ).foregroundStyle(
+                                .white
+                            ).font(
+                                .body1
+                            )
+                            
+                        }
+                    })
+                    .cornerRadius(
+                        10
+                    ).frame(
+                        height: 50.0
+                    )
+                }.containerRelativeFrame(
+                    .vertical
+                )
+                .textFieldStyle(
+                    .customRound(
+                        strokeSize: 1.5
+                    )
+                ).alert(tituloPreencherCampos, isPresented: $failedInput)
+                {
+                    Button("OK", role:.cancel, action: {})
                 }
-            }).cornerRadius(
-                10
-            ).frame(
-                height: 50.0
+                .keyboardType(
+                    .numberPad
+                ).bold()
+                    .padding()
+                    .foregroundStyle(
+                        Color.indigo600
+                    ).navigationTitle(
+                        "Cãoculadora"
+                    ).toolbarBackground(
+                        .visible,
+                        for: .navigationBar
+                    ).toolbarBackground(
+                        .indigo600,
+                        for: .navigationBar
+                    ).toolbarColorScheme(
+                        .dark,
+                        for: .navigationBar
+                    )
+            }.animation(.easeOut, value: result)
+            .scrollDismissesKeyboard(
+                .immediately
             )
         }
-        .textFieldStyle(
-            .customRound(
-                strokeSize: 1.5
-            )
-        )
-        //        .textFieldStyle(.roundedBorder)
-        .keyboardType(
-            .numberPad
-        )
-        .padding()
-        .foregroundStyle(
-            Color.indigo
-        )
     }
     func processYears(){
         
@@ -138,6 +176,7 @@ struct ContentView: View {
             print(
                 "Preencha o campo de entrada."
             )
+            failedInput = true
             return
         } 
         guard years > 0 || months > 0 else {
@@ -145,19 +184,22 @@ struct ContentView: View {
             print(
                 "Algum dos campos deve ser maior que zero."
             )
+            failedInput = true
             return
         }
-        let multiplicador: Int
-        result = porteSelecionado.idadeConvertida(
-            years: years,
-            months: months
-        )
+//        withAnimation(.easeIn.speed(0.5)){
+            result = porteSelecionado.idadeConvertida(
+                years: years,
+                months: months
+            )
+        //}
     }
 }
 
 #Preview {
     ContentView()
 }
+
 
 struct CustomStrokeTextFieldStyle: TextFieldStyle {
     let strokeSize: Double
@@ -180,7 +222,7 @@ struct CustomStrokeTextFieldStyle: TextFieldStyle {
                     style:.continuous
                 )
                 .stroke(
-                    Color.indigo.opacity(
+                    Color.indigo600.opacity(
                         0.3
                     ),
                     lineWidth: strokeSize
